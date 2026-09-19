443744c951d7b1552c27837852a2144e29e8172e
Abdulrahman Ali <abdulrahmanalhijaji@gmail.com>
2026-09-19 15:40:40 +0300

    fix the ready to hit the road color

--- frontend/src/components/layouts/ReadyToHitTheRoad.tsx
+++ frontend/src/components/layouts/ReadyToHitTheRoad.tsx
@@ -12,7 +12,7 @@ export const ReadyToHitTheRoad: React.FC = () => {
           <span>UNLOCK LIBYA&apos;S PREMIER ROADS</span>
         </div>
 
-        <h2 className="text-4xl sm:text-6xl font-black tracking-tight uppercase leading-[1.05] mb-6">
+        <h2 className="text-4xl sm:text-6xl font-black tracking-tight uppercase leading-[1.05] mb-6 text-white">
           READY TO HIT THE ROAD?
         </h2>
 
